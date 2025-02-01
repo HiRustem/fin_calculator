f0d63d0e09fd2776f7cce7fa32267c05f8a0ae2a
Rustem <rustem_hajrullin@enot.work>
2025-02-01 21:42:18 +0300

    fix vercel json

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/credit_calculator" element={<CreditCalculator />} />
-        <Route path="*" element={<Navigate to="credit_calculator" replace />} />
+        {/* <Route path="*" element={<Navigate to="credit_calculator" replace />} /> */}
       </Routes>
     </BrowserRouter>
   );
